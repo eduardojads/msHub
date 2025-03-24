b1a1063f616938aca98dfb15f0bb93e8754895d4
Laboratório FIAP <labsfiap@fiap.com.br>
2025-03-24 11:47:22 -0300

    feat: PagamentoController method create, PagamentoService createPagamento, add handlers

--- ms-pagamento/src/main/java/com/github/eduardojads/ms_pagamento/controller/PagamentoController.java
+++ ms-pagamento/src/main/java/com/github/eduardojads/ms_pagamento/controller/PagamentoController.java
@@ -2,12 +2,14 @@ package com.github.eduardojads.ms_pagamento.controller;
 
 import com.github.eduardojads.ms_pagamento.dto.PagamentoDTO;
 import com.github.eduardojads.ms_pagamento.service.PagamentoService;
+import jakarta.validation.Valid;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -21,5 +23,25 @@ public class PagamentoController {
     public ResponseEntity<List<PagamentoDTO>> getAll(){
         List<PagamentoDTO> dto = service.getAll();
         return ResponseEntity.ok(dto);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PagamentoDTO> getById(@PathVariable Long id){
+        PagamentoDTO dto = service.getById(id);
+
+        return ResponseEntity.ok(dto);
+    }
+
+    @PostMapping
+    public ResponseEntity<PagamentoDTO> create(@RequestBody @Valid PagamentoDTO dto){
+        dto = service.create(dto);
+
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequestUri()
+                .path("/{id}")
+                .buildAndExpand(dto.getId())
+                .toUri();
+
+       return ResponseEntity.created(uri).body(dto);
     }
 }
--- ms-pagamento/src/main/java/com/github/eduardojads/ms_pagamento/service/PagamentoService.java
+++ ms-pagamento/src/main/java/com/github/eduardojads/ms_pagamento/service/PagamentoService.java
@@ -2,6 +2,7 @@ package com.github.eduardojads.ms_pagamento.service;
 
 import com.github.eduardojads.ms_pagamento.dto.PagamentoDTO;
 import com.github.eduardojads.ms_pagamento.entity.Pagamento;
+import com.github.eduardojads.ms_pagamento.entity.Status;
 import com.github.eduardojads.ms_pagamento.repository.PagamentoRepository;
 import com.github.eduardojads.ms_pagamento.service.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,5 +33,27 @@ public class PagamentoService {
 
         return new PagamentoDTO(entity);
     }
+
+    @Transactional
+    public PagamentoDTO create(PagamentoDTO dto){
+        Pagamento entity = new Pagamento();
+        copyDtoToEntity(dto, entity);
+        entity.setStatus(Status.CRIADO);
+        entity = repository.save(entity);
+
+        return new PagamentoDTO(entity);
+    }
+
+    private void copyDtoToEntity(PagamentoDTO dto, Pagamento entity) {
+        entity.setValor(dto.getValor());
+        entity.setNome(dto.getNome());
+        entity.setNumeroDoCartao(dto.getNumeroDoCartao());
+        entity.setValidade(dto.getValidade());
+        entity.setCodigoDeSeguranca(dto.getCodigoDeSeguranca());
+        entity.setPedidoId(dto.getPedidoId());
+        entity.setFormaDePagamentoId(dto.getFormaDePagamentoId());
+
+    }
+
 
 }
