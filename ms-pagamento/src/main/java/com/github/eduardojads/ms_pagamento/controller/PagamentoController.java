c6ce16cb2efa61a5449864c41eff577151fb3071
Laboratório FIAP <labsfiap@fiap.com.br>
2025-03-24 11:11:59 -0300

    feat: add PagamentoController, method: getAll()

---
+++ ms-pagamento/src/main/java/com/github/eduardojads/ms_pagamento/controller/PagamentoController.java
@@ -0,0 +1,25 @@
+package com.github.eduardojads.ms_pagamento.controller;
+
+import com.github.eduardojads.ms_pagamento.dto.PagamentoDTO;
+import com.github.eduardojads.ms_pagamento.service.PagamentoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/pagamentos")
+public class PagamentoController {
+
+    @Autowired
+    private PagamentoService service;
+
+    @GetMapping
+    public ResponseEntity<List<PagamentoDTO>> getAll(){
+        List<PagamentoDTO> dto = service.getAll();
+        return ResponseEntity.ok(dto);
+    }
+}
