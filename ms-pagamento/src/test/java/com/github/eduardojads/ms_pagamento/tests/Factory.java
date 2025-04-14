26eaab0882b534dfe7a6f3eb5fa138331f507238
Laboratório FIAP <labsfiap@fiap.com.br>
2025-04-14 11:43:50 -0300

    Factory method: newPagamentoWithRequiredFields

--- ms-pagamento/src/test/java/com/github/eduardojads/ms_pagamento/tests/Factory.java
+++ ms-pagamento/src/test/java/com/github/eduardojads/ms_pagamento/tests/Factory.java
@@ -8,21 +8,31 @@ import java.math.BigDecimal;
 
 public class Factory {
 
-    public static Pagamento createPagamento(){
+    public static Pagamento createPagamento() {
         Pagamento pagamento = new Pagamento(1L, BigDecimal.valueOf(32.25),
                 "Jon Snow", "2364325423543621",
                 "07/32", "585", Status.CRIADO, 1L, 2L);
         return pagamento;
     }
 
-    public static PagamentoDTO createPagamentoDTO(){
+    public static PagamentoDTO createPagamentoDTO() {
         Pagamento pagamento = createPagamento();
         return new PagamentoDTO(pagamento);
     }
 
-    public static PagamentoDTO createNewPagamentoDTO(){
+    public static PagamentoDTO createNewPagamentoDTO() {
         Pagamento pagamento = createPagamento();
         pagamento.setId(null);
+        return new PagamentoDTO(pagamento);
+    }
+
+    public static PagamentoDTO createNewPagamentoDTOWithRequiredFields() {
+        Pagamento pagamento = createPagamento();
+        pagamento.setId(null);
+        pagamento.setNome(null);
+        pagamento.setNumeroDoCartao(null);
+        pagamento.setValidade(null);
+        pagamento.setCodigoDeSeguranca(null);
         return new PagamentoDTO(pagamento);
     }
 
