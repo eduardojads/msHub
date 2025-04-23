3203c37852726a7e9c6197df945a88d28d21c2c3
Laboratório FIAP <labsfiap@fiap.com.br>
2025-04-23 09:52:22 -0300

    Status - entity, pom

---
+++ ms-pedido/src/main/java/entities/Status.java
@@ -0,0 +1,13 @@
+package entities;
+
+public enum Status {
+
+    REALIZADO,
+    AGUARDANDO_PAGAMENTO,
+    PAGO,
+    CONFIRMADO,
+    PRONTO_PARA_ENTREGA,
+    SAIU_PARA_ENTREGA,
+    ENTREGUE,
+    CANCELADO
+}
