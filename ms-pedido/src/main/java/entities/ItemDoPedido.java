8c3781b3128335936cfe3447c1d9f00f36be4688
Laboratório FIAP <labsfiap@fiap.com.br>
2025-04-28 10:48:56 -0300

    feat: add aplication-test-properties

---
+++ ms-pedido/src/main/java/entities/ItemDoPedido.java
@@ -0,0 +1,25 @@
+package entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id")
+@Entity
+@Table(name = "tb_item_pedido")
+public class ItemDoPedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Integer quantidade;
+    private String descricao;
+    private BigDecimal valorUnitario;
+}
