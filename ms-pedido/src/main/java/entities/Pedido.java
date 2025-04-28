5e7eab91b0461be411e1af9dc910bd12f5c3e3a1
Laboratório FIAP <labsfiap@fiap.com.br>
2025-04-28 10:41:58 -0300

    feat: Pedido - Entity

---
+++ ms-pedido/src/main/java/entities/Pedido.java
@@ -0,0 +1,26 @@
+package entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id")
+@Entity
+@Table (name = "tb_pedido")
+public class Pedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String cpf;
+    private LocalDate data;
+    @Enumerated(EnumType.STRING)
+    private Status status;
+}
